1d58714979709ec6cde41ec7648b83648eb83b3c
Kaavya <kaavya446@gmail.com>
2023-03-31 10:42:09 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     <h1>Reference Datatypes use same memory
+     </h1>
+     <button onClick={window['print']}>Click here</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
